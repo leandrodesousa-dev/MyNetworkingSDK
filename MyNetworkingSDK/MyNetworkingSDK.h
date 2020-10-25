ae9bdc40c8f281b8affbd414f0516771ef0abc8e
Leandro <leandrooodesousa@gmail.com>
2020-10-25 15:24:35 -0400

    Initial Commit

---
+++ MyNetworkingSDK/MyNetworkingSDK.h
@@ -0,0 +1,18 @@
+//
+//  MyNetworkingSDK.h
+//  MyNetworkingSDK
+//
+//  Created by Leandro de Sousa Silva on 25/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyNetworkingSDK.
+FOUNDATION_EXPORT double MyNetworkingSDKVersionNumber;
+
+//! Project version string for MyNetworkingSDK.
+FOUNDATION_EXPORT const unsigned char MyNetworkingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyNetworkingSDK/PublicHeader.h>
+
+
